7a907eb764c3765b6ef45713bebcc874fedcb35e
Arun Vishnu <arunnampoothiri.vish@walmart.com>
2022-10-19 18:00:38 -0700

    feat: test publish

--- packages/subapp-web/src/index.js
+++ packages/subapp-web/src/index.js
@@ -1,6 +1,6 @@
 import { createBrowserHistory } from "history";
-import makeSubAppSpec from "./make-subapp-spec";
 import xarc from "./xarc";
+import makeSubAppSpec from "./make-subapp-spec";
 
 export { default as makeSubAppSpec } from "./make-subapp-spec";
 
