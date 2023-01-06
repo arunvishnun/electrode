e451e7b2bbe7009bc42cb4adc4a7abde6ebd862b
arunvishnun <1584121+arunvishnun@users.noreply.github.com>
2023-01-06 15:58:18 -0800

    feat: experimental feature for rush changelog creation
    An experimental feature for rush changelog creation from conventional commits utilizing commitlint with autoinstallers

---
+++ common/scripts/rush-changefiles-utils.js
@@ -0,0 +1,139 @@
+const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const node_modules = path.join(__dirname, '..', 'autoinstallers/rush-changemanager/node_modules');
+const rushLib = require(path.join(node_modules, '@microsoft/rush-lib'));
+const rushCore = require(path.join(node_modules, '@rushstack/node-core-library'));
+
+class RushUtil {
+
+    constructor() {
+        this.rushConfiguration = rushLib.RushConfiguration.loadFromDefaultLocation({ startingFolder: process.cwd() })
+    }
+    get getChangesFolder() {
+        return this.rushConfiguration.changesFolder;
+    }
+    get getTempFolder() {
+        return this.rushConfiguration.commonTempFolder;
+    }
+    get getHotfixChangeEnabled() {
+        return this.rushConfiguration.hotfixChangeEnabled;
+    }
+    get getRemoteDefaultBranch() {
+        return `${this.rushConfiguration.repositoryDefaultRemote}/${this.rushConfiguration.repositoryDefaultBranch}`;
+    }
+    get getDefaultRemote() {
+        return this.rushConfiguration.repositoryDefaultRemote;
+    }
+    async getChangedProjectsAsync(currentBranch) {
+        const projectAnalyzer = new rushLib.ProjectChangeAnalyzer(this.rushConfiguration);
+        const terminal = new rushCore.Terminal(new rushCore.ConsoleTerminalProvider({ verboseEnabled: false }));
+
+        const changedProjects = await projectAnalyzer.getChangedProjectsAsync({
+            targetBranchName: currentBranch,
+            terminal: terminal,
+            enableFiltering: false,
+            includeExternalDependencies: false
+        });
+        return changedProjects;
+    }
+}
+
+class Util {
+
+    Colors = {
+        Red: '\u001B[31m',
+        Purple: '\u001B[35m',
+        Green: '\u001B[32m',
+        Yellow: '\u001B[33m',
+        Gray: '\u001B[30;1m',
+        Reset: '\u001B[0m'
+    };
+    getArg(argName) {
+        const yargs = require(path.join(node_modules, 'yargs'));
+        return yargs(process.argv).argv[argName];
+    }
+    executeCommand(command) {
+        //stdio: 'inherit': process will use the parent's stdin, stdout and stderr streams
+        return child_process.execSync(command, { stdio: 'inherit' });
+    }
+    executeCommandReturn(command) {
+        //stdio: 'inherit': process will use the parent's stdin, stdout and stderr streams
+        return child_process.execSync(command).toString().trim();;
+    }
+    executeCommandAsync(command) {
+        //stdio: 'inherit': process will use the parent's stdin, stdout and stderr streams
+        return child_process.exec(command, { stdio: 'inherit' });
+    }
+    spawnCommandReturn(command, args) {
+        const escapedArgs = args.map((x) => this._escapeShellParameter(x));
+        const result = child_process.spawnSync(command, escapedArgs, {
+            shell: true,
+            cwd: process.cwd(),
+            env: process.env,
+            stdio: 'pipe'
+        });
+        this._processResult(result);
+        return result.stdout.toString();
+    }
+    detectEmail() {
+        try {
+            return this.executeCommandReturn('git config user.email').replace(/(\r\n|\n|\r)/gm, '');
+        } catch (err) {
+            console.log('There was an issue detecting your Git email...');
+            return undefined;
+        }
+    }
+    detectName() {
+        try {
+            return this.executeCommandReturn('git config user.name').replace(/(\r\n|\n|\r)/gm, '');
+        } catch (err) {
+            console.log('There was an issue detecting your Git name...');
+            return undefined;
+        }
+    }
+    getmergeHash(targetBranch) {
+        return this.executeCommandReturn(`git --no-optional-locks merge-base -- HEAD ${targetBranch}`);
+    }
+    getChangeFileNamePrefix() {
+        const currentBranch = child_process.execSync("git branch --show-current").toString().trim();
+        return this.escapeFilename(currentBranch);
+    }
+
+    getCurrentBranch(defaultRemote) {
+        // const defaultRemote = rushConfiguration.repositoryDefaultRemote;
+        const currBranch = child_process.execSync("git branch --show-current").toString().trim();
+        try {
+            return child_process.execSync(`git rev-parse --symbolic-full-name --abbrev-ref "${currBranch}@{u}"`).toString().trim();
+        } catch (error) {
+            console.log(this.Colors.Red + "Error fetching git remote branch features/versions. Detected changed files may be incorrect." + this.Colors.Reset)
+            console.log(this.Colors.Yellow + `Execute 'git push --set-upstream ${defaultRemote} ${currBranch}' or 'git checkout --track ${defaultRemote}/${currBranch}' to set upstream branch` + this.Colors.Reset);
+            return null;
+        }
+    }
+    escapeFilename(filename, replacer) {
+        // Removes / ? < > \ : * | ", really anything that isn't a letter, number, '.' '_' or '-'
+        const badCharacters = /[^a-zA-Z0-9._-]/g;
+        return filename.replace(badCharacters, replacer ?? '-');
+    }
+
+    _escapeShellParameter(parameter) {
+        // This approach is based on what NPM 7 now does:
+        // https://github.com/npm/run-script/blob/47a4d539fb07220e7215cc0e482683b76407ef9b/lib/run-script-pkg.js#L34
+        return JSON.stringify(parameter);
+    }
+    _processResult(result) {
+        //rushstack\libraries\rush-lib\src\utilities\Utilities.ts
+        if (result.error) {
+            result.error.message += os.EOL + (result.stderr ? result.stderr.toString() + os.EOL : '');
+            throw result.error;
+        }
+
+        if (result.status) {
+            throw new Error('The command failed with exit code ' + result.status + os.EOL + (result.stderr ? result.stderr.toString() : ''));
+        }
+    }
+}
+
+exports.utils = new Util();
+exports.rushUtils = new RushUtil();
---
+++ common/scripts/rush-whatChanged.js
@@ -0,0 +1,181 @@
+const fs = require("fs");
+const path = require('path');
+const { utils, rushUtils } = require('./rush-changefiles-utils.js');
+const node_modules = path.join(__dirname, '..', 'autoinstallers/rush-changemanager/node_modules');
+
+
+async function rushChangeInfo(_showCommitsParam, _recommendChangetypeParam, _targetBranchParam) {
+
+    function printChangesSummary(projectName, since, commitsCount, stagedChangesCount) {
+        const printSince = (since !== undefined) ? ` since ${since}` : ''
+        console.log('\n' + utils.Colors.Yellow + `- ${projectName}` + utils.Colors.Reset + `${printSince}: commits: ${commitsCount}, staged files: ${stagedChangesCount}`)
+    }
+
+    function printRecommendedChangeType(changeType, info) {
+        const infoMsg = (info) ? ` (${info})` : '';
+        console.log(`  recommended change type: ` + utils.Colors.Green + changeType + utils.Colors.Reset + infoMsg);
+    }
+    function printCommits(logFormat, mergeBaseHash, since, changeFileName, projectRelativeFolder, targetPath) {
+
+        const getCommitsHeader = (changeFileName === undefined) ? `commits history` : `commits history since '${changeFileName}'`;
+        switch (logFormat) {
+            case 'full':
+                utils.executeCommand(`git --no-pager log ${mergeBaseHash}... ${since} -- "${projectRelativeFolder}" > ${targetPath}`);
+                console.log(`  ${getCommitsHeader}: '` + utils.Colors.Yellow + targetPath + utils.Colors.Reset + `'`)
+                break;
+            case 'shortlog':
+                console.log(`  ${getCommitsHeader}: `)
+                utils.executeCommand(`git shortlog ${mergeBaseHash}... ${since} -- "${projectRelativeFolder}"`);
+                break;
+        }
+    }
+    function getChangeFiles(targetBranch) {
+        const changesFolder = rushUtils.getChangesFolder;
+        const currentBranchEncoded = utils.getChangeFileNamePrefix();
+        const pathFilter = `${changesFolder}\\*\\${currentBranchEncoded}_*`;
+        console.log(pathFilter)
+
+        const changeFiles = utils.executeCommandReturn(`git diff ${targetBranch}... --name-only --no-renames --diff-filter=A -- "${pathFilter}"`).split('\n');
+
+        const result = changeFiles.reduce((acc, obj) => {
+            const parsed = path.parse(path.relative(changesFolder, obj));
+            const key = parsed.dir;
+
+            acc[key] = (acc[key]) ? [acc[key], parsed.base].reverse()[0] : parsed.base;
+
+            return acc;
+        }, {})
+
+        return result;
+    }
+    function getDateFromChangeFile(fileName, format) {
+        if (fileName === undefined) {
+            return '';
+        }
+
+        //always branchName_YYYY-MM-DD-HH-MM.json OR branchName_YYYY-MM-DD-HH-MM-SS.json
+        const res = fileName.match("_(?<dateTime>.*).json")
+        const val = res.groups.dateTime.split('-');
+
+        if (format == 'git') {
+            return `--since=${val[0]}-${val[1]}-${val[2]}T${val[3]}:${val[4]}:${val[5] ?? '00'}`;
+        }
+        else {
+            return `${val[0]}-${val[1]}-${val[2]} ${val[3]}:${val[4]}:${val[5] ?? '00'}`;
+        }
+    }
+    function getCommitsCount(_mergeBaseHash, _since, _projectRelativeFolder) {
+        return utils.executeCommandReturn(`git rev-list ${_mergeBaseHash}... --count ${_since} -- "${_projectRelativeFolder}"`);
+    }
+    function getStagedInfo(_changedProjects) {
+        let result = {}
+        _changedProjects.forEach(project => {
+            // utils.executeCommand(`git diff --cached --stat -- "${project.projectRelativeFolder}"`);
+            result[project.packageName] = utils.executeCommandReturn(`git diff --cached --name-only -- "${project.projectRelativeFolder}"`).split('\n').length;
+        });
+        return result;
+    }
+    //---
+    function showCommits(_logFormat, _mergeHash, _since, _project, _commitsCount, _changeFileName) {
+
+        function _ensureDestinationFolder() {
+            const targetFolder = path.join(rushUtils.getTempFolder, "gitlog");
+            fs.mkdirSync(targetFolder, { recursive: true });
+            return targetFolder;
+        }
+
+        if (_commitsCount > 0) {
+            const destFolder = _ensureDestinationFolder();
+            const fileName = _project.projectRelativeFolder.replace('/', '_').replace('\\', '_')
+            printCommits(_logFormat, _mergeHash, _since, _changeFileName, _project.projectRelativeFolder, path.join(destFolder, `${fileName}.txt`))
+        }
+    }
+
+    function recommendChangeType(_mergeHash, _project, _commitsCount) {
+        function _getChangeType(mergeHash, projectFolder) {
+            function _getRevListRegex(mergeCommitHash, regex, projectFolder) {
+                return [
+                    'rev-list',
+                    `${mergeCommitHash}...`,
+                    '--count',
+                    '--extended-regexp',
+                    '--grep',
+                    regex,
+                    '--',
+                    projectFolder
+                ];
+            }
+            function _isMajor(mergeCommitHash, projectFolder) {
+                const cctypes = require(path.join(node_modules, 'conventional-commit-types'));
+                const types = Object.keys(cctypes.types).join('|');
+                const result = utils.spawnCommandReturn('git', _getRevListRegex(mergeCommitHash, `(^(${types})(\(.*?\))?!:.*|^BREAKING CHANGE: )`, projectFolder))
+                return parseInt(result) > 0;
+            }
+            function _isMinor(mergeCommitHash, projectFolder) {
+                const result = utils.spawnCommandReturn('git', _getRevListRegex(mergeCommitHash, '^feat((.*?))?:', projectFolder))
+                return parseInt(result) > 0;
+            }
+            function _isPatch(mergeCommitHash, projectFolder) {
+                const result = utils.spawnCommandReturn('git', _getRevListRegex(mergeCommitHash, '^fix((.*?))?:', projectFolder))
+                return parseInt(result) > 0;
+            }
+
+            if (utils.hotfixChangeEnabled) {
+                return { changeType: "none, hotfix", info: "if 'hotfixChangeEnabled' is set in rush.json, 'rush change' only allows a 'hotfix' change types" }
+
+            }
+            else if (_isMajor(mergeHash, projectFolder)) {
+                return { changeType: "major", info: "" };
+            }
+            else if (_isMinor(mergeHash, projectFolder)) {
+                return { changeType: "minor", info: "" };
+            }
+            else if (_isPatch(mergeHash, projectFolder)) {
+                return { changeType: "patch", info: "" };
+            }
+            else {
+                return { changeType: "none", info: "" };
+            }
+        }
+
+        const { changeType, info } = (_commitsCount == 0) ?
+            { changeType: "N/A", info: "there are no commits to parse" } :
+            _getChangeType(_mergeHash, _project.projectRelativeFolder);
+
+        printRecommendedChangeType(changeType, info);
+    }
+
+    const targetBranch = _targetBranchParam || rushUtils.getRemoteDefaultBranch;
+    const mergeHash = utils.getmergeHash(targetBranch);
+
+    const changedProjects = await rushUtils.getChangedProjectsAsync(targetBranch);  //RushConfigurationProject
+    const changeFiles = getChangeFiles(targetBranch);                               //{'packageName':'<branchname>-<timestamp>.json','packageName':'<branchname>-<timestamp>.json' }
+    const stagedFiles = getStagedInfo(changedProjects);                             //{'packageName':count,'packageName':count }
+
+    if (changedProjects.size == 0) {
+        console.log('No changes were detected to relevant packages on this branch. Nothing to do.');
+        return;
+    }
+    changedProjects.forEach(project => {
+
+        const since = getDateFromChangeFile(changeFiles[project.packageName], 'git');
+        const sinceInfo = getDateFromChangeFile(changeFiles[project.packageName]);
+        const commitsCount = getCommitsCount(mergeHash, since, project.projectRelativeFolder)
+
+        printChangesSummary(project.projectRelativeFolder, sinceInfo, commitsCount, stagedFiles[project.packageName]);
+
+        if (_recommendChangetypeParam !== undefined) {
+            recommendChangeType(mergeHash, project, commitsCount)
+        }
+        if (_showCommitsParam !== undefined) {
+            showCommits(_showCommitsParam, mergeHash, since, project, commitsCount, changeFiles[project.packageName]);
+        }
+
+    });
+}
+
+rushChangeInfo(
+    utils.getArg("showCommits"),
+    utils.getArg("recommendChangetype"),
+    utils.getArg("targetBranch")
+)
